e17ef8f3939cdcff67c2b4b678fded86eaed5578
Todd Yamakawa <tyamakawa@tenstorrent.com>
2023-05-26 17:04:03 -0500

    setup.sh: Adding jq and skhd.

--- setup.sh
+++ setup.sh
@@ -1,7 +1,24 @@
 #!/usr/bin/env bash
 if ! command -v yabai &>/dev/null; then
+	echo
 	print-cmd brew install koekeishiya/formulae/yabai
-	yesno || exit $?
-	brew install koekeishiya/formulae/yabai
+	if yesno; then
+		brew install koekeishiya/formulae/yabai
+	fi
 fi
 
+if ! command -v jq &>/dev/null; then
+	echo
+	print-cmd brew install jq
+	if yesno; then
+		brew install jq
+	fi
+fi
+
+if ! command -v skhd &>/dev/null; then
+	echo
+	print-cmd brew install koekeishiya/formulae/skhd
+	if yesno; then
+		brew install koekeishiya/formulae/skhd
+	fi
+fi
