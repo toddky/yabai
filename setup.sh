f36309516e4e6e6006bc0e0089227e0a29a3d084
Todd Yamakawa <tyamakawa@tenstorrent.com>
2024-12-26 15:45:02 -0600

    setup.sh: Updating sudoers.

--- setup.sh
+++ setup.sh
@@ -22,3 +22,12 @@ if ! command -v skhd &>/dev/null; then
 		brew install koekeishiya/formulae/skhd
 	fi
 fi
+
+# Configure scripting addition
+# https://github.com/koekeishiya/yabai/wiki/Installing-yabai-(latest-release)#configure-scripting-addition
+yabai="$(which yabai)"
+sha256="$(shasum -a 256 "$yabai" | cut -d' ' -f1)"
+echo "$yabai $sha256"
+sudoer_line="$(echo "$(whoami) ALL=(root) NOPASSWD: sha256:$sha256 $(which yabai) --load-sa")"
+echo "$sudoer_line" | sudo tee /private/etc/sudoers.d/yabai
+
